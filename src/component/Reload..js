691cdb7ab587c3ef4ad75191bb8c7baf5c5ca3db
Meanborey <99602111+Meanborey@users.noreply.github.com>
2023-04-20 01:17:23 +0700

    push homework react

--- src/component/Reload..js
+++ src/component/Reload..js
@@ -111,6 +111,150 @@ function PlaceHolder() {
                             </div>
                         </div>
                     </div>
+                    <div className='col-12 col-sm-6 col-md-4 col-xl-3'>
+                        <div className="card" aria-hidden="true">
+                            <img src="https://via.placeholder.com/640x360" className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title placeholder-glow">
+                                    <span className="placeholder col-6"></span>
+                                </h5>
+                                <p className="card-text placeholder-glow">
+                                    <span className="placeholder col-7"></span>
+                                    <span className="placeholder col-4"></span>
+                                    <span className="placeholder col-4"></span>
+                                    <span className="placeholder col-6"></span>
+                                    <span className="placeholder col-8"></span>
+                                </p>
+                                <a class="btn btn-primary disabled placeholder col-6"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-12 col-sm-6 col-md-4 col-xl-3'>
+                        <div className="card" aria-hidden="true">
+                            <img src="https://via.placeholder.com/640x360" className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title placeholder-glow">
+                                    <span className="placeholder col-6"></span>
+                                </h5>
+                                <p className="card-text placeholder-glow">
+                                    <span className="placeholder col-7"></span>
+                                    <span className="placeholder col-4"></span>
+                                    <span className="placeholder col-4"></span>
+                                    <span className="placeholder col-6"></span>
+                                    <span className="placeholder col-8"></span>
+                                </p>
+                                <a class="btn btn-primary disabled placeholder col-6"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-12 col-sm-6 col-md-4 col-xl-3'>
+                        <div className="card" aria-hidden="true">
+                            <img src="https://via.placeholder.com/640x360" className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title placeholder-glow">
+                                    <span className="placeholder col-6"></span>
+                                </h5>
+                                <p className="card-text placeholder-glow">
+                                    <span className="placeholder col-7"></span>
+                                    <span className="placeholder col-4"></span>
+                                    <span className="placeholder col-4"></span>
+                                    <span className="placeholder col-6"></span>
+                                    <span className="placeholder col-8"></span>
+                                </p>
+                                <a class="btn btn-primary disabled placeholder col-6"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-12 col-sm-6 col-md-4 col-xl-3'>
+                        <div className="card" aria-hidden="true">
+                            <img src="https://via.placeholder.com/640x360" className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title placeholder-glow">
+                                    <span className="placeholder col-6"></span>
+                                </h5>
+                                <p className="card-text placeholder-glow">
+                                    <span className="placeholder col-7"></span>
+                                    <span className="placeholder col-4"></span>
+                                    <span className="placeholder col-4"></span>
+                                    <span className="placeholder col-6"></span>
+                                    <span className="placeholder col-8"></span>
+                                </p>
+                                <a class="btn btn-primary disabled placeholder col-6"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-12 col-sm-6 col-md-4 col-xl-3'>
+                        <div className="card" aria-hidden="true">
+                            <img src="https://via.placeholder.com/640x360" className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title placeholder-glow">
+                                    <span className="placeholder col-6"></span>
+                                </h5>
+                                <p className="card-text placeholder-glow">
+                                    <span className="placeholder col-7"></span>
+                                    <span className="placeholder col-4"></span>
+                                    <span className="placeholder col-4"></span>
+                                    <span className="placeholder col-6"></span>
+                                    <span className="placeholder col-8"></span>
+                                </p>
+                                <a class="btn btn-primary disabled placeholder col-6"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-12 col-sm-6 col-md-4 col-xl-3'>
+                        <div className="card" aria-hidden="true">
+                            <img src="https://via.placeholder.com/640x360" className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title placeholder-glow">
+                                    <span className="placeholder col-6"></span>
+                                </h5>
+                                <p className="card-text placeholder-glow">
+                                    <span className="placeholder col-7"></span>
+                                    <span className="placeholder col-4"></span>
+                                    <span className="placeholder col-4"></span>
+                                    <span className="placeholder col-6"></span>
+                                    <span className="placeholder col-8"></span>
+                                </p>
+                                <a class="btn btn-primary disabled placeholder col-6"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-12 col-sm-6 col-md-4 col-xl-3'>
+                        <div className="card" aria-hidden="true">
+                            <img src="https://via.placeholder.com/640x360" className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title placeholder-glow">
+                                    <span className="placeholder col-6"></span>
+                                </h5>
+                                <p className="card-text placeholder-glow">
+                                    <span className="placeholder col-7"></span>
+                                    <span className="placeholder col-4"></span>
+                                    <span className="placeholder col-4"></span>
+                                    <span className="placeholder col-6"></span>
+                                    <span className="placeholder col-8"></span>
+                                </p>
+                                <a class="btn btn-primary disabled placeholder col-6"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-12 col-sm-6 col-md-4 col-xl-3'>
+                        <div className="card" aria-hidden="true">
+                            <img src="https://via.placeholder.com/640x360" className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title placeholder-glow">
+                                    <span className="placeholder col-6"></span>
+                                </h5>
+                                <p className="card-text placeholder-glow">
+                                    <span className="placeholder col-7"></span>
+                                    <span className="placeholder col-4"></span>
+                                    <span className="placeholder col-4"></span>
+                                    <span className="placeholder col-6"></span>
+                                    <span className="placeholder col-8"></span>
+                                </p>
+                                <a class="btn btn-primary disabled placeholder col-6"></a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
